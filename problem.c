834708b4de813fdb444ae4d83e8e34016b2b87b8
Viv.CD <dumitrescucarla2@gmail.com>
2023-04-27 16:57:16 +0300

    the 3 meniu function and 2 obtion proccesing funct

--- problem.c
+++ problem.c
@@ -1,1 +1,200 @@
-danksldnjaslk
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <dirent.h>
+
+void printMenu1() {
+    printf("----FILE-MENU----\n");
+    printf("\tt - time of last modification\n");
+    printf("\tl - create symbolic link\n");
+    printf("\tn - name\n");
+    printf("\td - size\n");
+    printf("\ta - access rights\n");
+    printf("\th - hard link count\n");
+    printf("Please enter your options.\n");
+    printf("The options should be preceded by a '-'\n");
+}
+
+void printMenu2() {
+    printf("----DIRECTORY-MENU----\n");
+    printf("\tc - number of C file entries\n");
+    printf("\td - size\n");
+    printf("\tn - name\n");
+    printf("\ta - access rights\n");
+    printf("Please enter your options.\n");
+    printf("The options should be preceded by a '-'\n");
+}
+
+void printMenu3() {
+    printf("----LINK-MENU----\n");
+    printf("\td - size\n");
+    printf("\tn - name\n");
+    printf("\tl - delete\n");
+    printf("\ta - access rights\n");
+    printf("\tt - size of target file\n");
+    printf("Please enter your options.\n");
+    printf("The options should be preceded by a '-'\n");
+}
+
+void printAccessRights(struct stat status, char *filePath) {
+    printf("\nAccess Rights for %s:\n", filePath);
+
+    char *groups[] = {"User", "Group", "Others"};
+    int modes[] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
+    char *permissions[] = {"Read", "Write", "Execute"};
+
+    for (int i = 0; i < 3; i++) {
+        printf("%s:\n", groups[i]);
+        for (int j = 0; j < 3; j++) {
+            int k = i * 3 + j;
+            printf("\t%s - %s\n", permissions[j], (status.st_mode & modes[k]) ? "yes" : "no");
+        }
+        printf("\n");
+    }
+}
+void processFileOptions(struct stat status, char *filePath) {
+    const int MAX_OPTIONS = 5;
+    const char* VALID_OPTIONS = "ndhmal";
+    char options[MAX_OPTIONS + 1];
+    int isValid = 0;
+
+    do {
+        if (!isValid) {
+            printFileMenu();
+        }
+        
+        if (scanf("%5s", options) != 1) {
+            perror("Scanf failed.\n");
+        }
+
+        isValid = 1;
+        int nrOfOptions = strlen(options);
+
+        if (nrOfOptions < 2 || options[0] != '-') {
+            printf("Please enter the correct options.\n");
+            isValid = 0;
+            continue;
+        }
+        
+        for (int i = 1; i < nrOfOptions; i++) {
+            if (strchr(VALID_OPTIONS, options[i]) == NULL) {
+                printf("Please enter the correct options.\n");
+                isValid = 0;
+                break;
+            }
+        }
+    } while (!isValid);
+
+    for (int i = 1; i < strlen(options); i++) {
+        switch (options[i]) {
+            case 'n':
+                printf("Name of file: %s\n", filePath);
+                break;
+            case 'd':
+                printf("File size: %ld bytes\n", status.st_size);
+                break;
+            case 'h':
+                printf("Hard link count: %ld\n", status.st_nlink);
+                break;
+            case 'm':
+                printf("Time of last modification: %s\n", ctime(&status.st_mtime));
+                break;
+            case 'a':
+                printf("Access rights:\n");
+                printAccessRights(status, filePath);
+                break;
+            case 'l':
+                printf("Please enter the link name: ");
+                char linkname[256];
+                if (scanf("%255s", linkname) != 1) {
+                    perror("Scanf failed.\n");
+                } else {
+                    if (symlink(filePath, linkname) == -1) {
+                        perror("Failed to create the symbolic link.\n");
+                    } else {
+                        printf("Symbolic link was created: from %s to %s\n", linkname, filePath);
+                    }
+                }
+                break;
+            default:
+                printf("Invalid option: %c\n", options[i]);
+                break;
+        }
+    }
+}
+
+void processLinkOptions(struct stat status, char *filePath) {
+    const int MAX_OPTIONS = 5;
+    const char* VALID_OPTIONS = "ndtal";
+    char options[MAX_OPTIONS + 1];
+    int isValid = 0;
+
+    do {
+        if (!isValid) {
+            printLinkMenu();
+        }
+        
+        if (scanf("%5s", options) != 1) {
+            perror("Scanf failed.\n");
+        }
+
+        isValid = 1;
+        int nrOfOptions = strlen(options);
+
+        if (nrOfOptions < 2 || options[0] != '-') {
+            printf("Please enter the correct options.\n");
+            isValid = 0;
+            continue;
+        }
+        
+        for (int i = 1; i < nrOfOptions; i++) {
+            if (strchr(VALID_OPTIONS, options[i]) == NULL) {
+                printf("Please enter the correct options.\n");
+                isValid = 0;
+                break;
+            }
+        }
+    } while (!isValid);
+
+    for (int i = 1; i < strlen(options); i++) {
+        switch (options[i]) {
+            case 'n':
+                printf("Name of link: %s\n", filePath);
+                break;
+            case 'd':
+                printf("Link size: %ld bytes\n", status.st_size);
+                break;
+            case 't':
+                struct stat targetSt;
+                if (stat(filePath, &targetSt) == -1) {
+                    perror("Stat failed.\n");
+                    break;
+                }
+                printf("Target file size: %ld bytes\n", targetSt.st_size);
+                break;
+            case 'a':
+                printf("Access rights:\n");
+                printAccessRights(status, filePath);
+                break;
+            case 'l':
+                printf("Deleting link...\n");
+                if (unlink(filePath) == -1) {
+                    perror("Failed to remove the link.\n");
+                } else {
+                    printf("Link removed successfully.\n");
+                }
+                break;
+            default:
+                printf("Invalid option: %c\n", options[i]);
+                break;
+        }
+    }
+}
+
+
+
+
